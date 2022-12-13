334ec0aec432669c9c75c3bf3db1efac0a8afe8b
Annaneilan <90556160+Annaneilan@users.noreply.github.com>
2022-12-13 19:30:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get('/', (request, response) => {
- response.status(200).json({
-   message: 'Hello Docker'
- })
+  response.status(200).json({
+    message: 'App is running'
+  })
 })
 
 app.listen(PORT, () => {
- console.log(`Server is up on localhost:${PORT}`)
+  console.log(`Server is up on localhost:${PORT}`)
 })
